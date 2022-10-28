bd2a66f99235741128c376a42194b8656fe2e549
muhammeadfarhansohail <msohail11@my.centennialcollege.ca>
2022-10-28 15:50:50 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/emp",
+  URI: "mongodb+srv://Muhammad:muhammad@cluster0.qud4eel.mongodb.net/Emp?retryWrites=true&w=majority",
+};
